16e78187247829fd5edfd5af3e479f0f65cd8ff7
Hessam Mehr <hessammehr@gmail.com>
2015-11-28 19:11:12 -0800

    Improved model, detects clicking on/near nodes, new nodes numbered sequentially.

--- src/fw1/core.cljs
+++ src/fw1/core.cljs
@@ -3,44 +3,88 @@
 
 (enable-console-print!)
 
-(println "Hey!")
+(declare max-node-id distance-squared node-position nearest-node
+  distance-squared distance node-click)
 
-;; define your app data so that it doesn't get over-written on reload
+(def node-radius 3)
+(def node-click-radius 8)
 
 (def app-state (atom {:nodes
-                      { 0 {:x 10 :y 15}
-                        1 {:x 90 :y 50}}
+                      { 0 {:pos [10 15] :id 0}
+                        1 {:pos [90 50] :id 1}
+                        2 {:pos [90 120] :id 2}}
                       :bonds
-                      { 0 {:nodes [0 1]}}}))
+                      { 0 {:nodes #{0 1} :id 1}
+                        1 {:nodes #{1 2} :id 2}}}))
 
-(defn add-node [x y] (swap! app-state assoc-in
-                      [:nodes (rand-int 1000)]
-                      {:x x :y y}))
+(defn max-node-id []
+  (->> (:nodes @app-state)
+      keys
+      (apply max)))
 
-(defn handle-click [ev]
+(defn node-position [node]
+  (get-in node [:pos]))
+
+(defn add-node! [{id :id :as node :or {id (+ (max-node-id) 1)}}]
+  ; (let ;[new-id (+ (max-node-id) 1)
+  ;       node
+      (swap! app-state assoc-in
+        [:nodes id] node))
+
+(defn canvas-click [ev]
   (let [x (- (aget ev "pageX") 8)
-        y (- (aget ev "pageY") 8)]
-      (add-node x y)))
+        y (- (aget ev "pageY") 8)
+        nearest-node (nearest-node [x y])
+        nn-distance (distance (:pos nearest-node) [x y])]
+      (if (> nn-distance node-click-radius)
+        (add-node! {:pos [x y]})
+        (node-click nearest-node))))
 
-(defn draw []
+(defn node-click [node]
+  (println "Node" node "clicked!"))
+
+(defn bond [{[x1 y1] :pos} {[x2 y2] :pos}]
+    [:line {:x1 x1 :y1 y1
+            :x2 x2 :y2 y2
+            :class "bond"}])
+
+(defn node [{[x y] :pos id :id}]
+  [:circle {:cx x :cy y :r node-radius
+            :on-click (fn [ev] (node-click id))}])
+
+(defn get-bonds [node]
+  (->> (:bonds @app-state)
+      seq
+      (filter #(contains? (:nodes (second %)) node))
+      (into {})))
+
+; Number of bonds to node not counting implicit hydrogens
+(defn explicit-valence [node]
+  (count (get-bonds node)))
+
+(defn nearest-node [[x y]]
+  (->> (:nodes @app-state)
+      vals
+      (apply min-key #(distance-squared [x y] (:pos %)))))
+
+(defn distance-squared [[x1 y1] [x2 y2]]
+  (+ (Math/pow (- x1 x2) 2)
+    (Math/pow (- y1 y2) 2)))
+(defn distance [p1 p2]
+  (Math.sqrt (distance-squared p1 p2)))
+
+(defn editor []
   (let [{nodes :nodes bonds :bonds} @app-state]
-    (conj [:svg {:on-click (fn [ev] (handle-click ev))}]
-      (for [[id {x :x y :y}] nodes]
-        [:circle {:cx x :cy y
-                  :r 3 :key id}])
-      (for [[id {[b1 b2] :nodes}] bonds]
-        (let [n1 (get nodes b1) n2 (get nodes b2)]
-          [:line {:x1 (:x n1)
-                  :y1 (:y n1)
-                  :x2 (:x n2)
-                  :y2 (:y n2)
-                  :class "bond"
-                  :key id}])))))
+    (conj [:svg {:on-click canvas-click}]
+      (for [[id n] nodes]
+        ^{:key id}[node n])
+      (for [[id {n :nodes}] bonds
+            :let [[n1 n2] (map nodes (seq n))]]
+          ^{:key id}[bond n1 n2]))))
 
+;(println (draw))
 
-(println (draw))
-
-(reagent/render-component [draw]
+(reagent/render-component [editor]
                           (. js/document (getElementById "app")))
 
 
