6447105ea68f991ade4cd906d37717a2e400099a
Hessam Mehr <hessammehr@gmail.com>
2017-10-24 18:58:30 -0700

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
-                 [org.clojure/clojurescript "1.9.494"]
-                 [org.clojure/core.async "0.3.442"]
-                 [reagent "0.6.1"]
-                 [com.rpl/specter "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/core.async "0.3.443"]
+                 [reagent "0.8.0-alpha2"]
+                 [com.rpl/specter "1.0.4"]]
                  
 
   :plugins [[lein-cljsbuild "1.1.5"]
