20f61c9cd8f2f95148b035623912db9974bc0398
Hessam Mehr <hessammehr@gmail.com>
2020-04-25 12:58:35 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -5,17 +5,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.312"]
-                 [org.clojure/core.async "0.4.474"]
+                 [org.clojure/clojurescript "1.10.520"]
                  [reagent "0.8.1"]
-                 [com.rpl/specter "1.1.1"]]
+                 [com.rpl/specter "1.1.3"]]
                  
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
   :resource-paths ["resources" "target"]
 
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.5"]
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.3"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]}}
 
   :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
