d8691fe6aac5b57fd4350aaa3b59294638454289
Hessam Mehr <hessammehr@gmail.com>
2020-04-25 12:58:26 +0100

    Updated dependencies and moved to figwheel-main.
    Run with `lein fig:build`.

--- project.clj
+++ project.clj
@@ -4,81 +4,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
-                 [org.clojure/clojurescript "1.9.946"]
-                 [org.clojure/core.async "0.3.443"]
-                 [reagent "0.8.0-alpha2"]
-                 [com.rpl/specter "1.0.4"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.312"]
+                 [org.clojure/core.async "0.4.474"]
+                 [reagent "0.8.1"]
+                 [com.rpl/specter "1.1.1"]]
                  
-
-  :plugins [[lein-cljsbuild "1.1.5"]
-            [lein-figwheel "0.5.9"]]
-
-  :source-paths ["src"]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
-  :cljsbuild {:builds
-              [{:id "dev"
-                :source-paths ["src"]
+  :resource-paths ["resources" "target"]
 
-                :figwheel {:on-jsload "molsketch-cljs.core/on-js-reload"}
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.5"]
+                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]}}
 
-                :compiler {:main molsketch-cljs.core
-                           :asset-path "js/compiled/out"
-                           :output-to "resources/public/js/compiled/out/molsketch-cljs.js"
-                           :output-dir "resources/public/js/compiled/out"
-                           :source-map true
-                           :optimizations :none
-                           :source-map-timestamp true}}
-               ;; This next build is an compressed minified build for
-               ;; production. You can build this with:
-               ;; lein cljsbuild once min
-               {:id "min"
-                :source-paths ["src"]
-                :compiler {:output-to "resources/public/js/compiled/molsketch-cljs.js"
-                           :main molsketch-cljs.core
-                           :optimizations :advanced
-                           :pretty-print false}}
-               {:id "test"
-                 :source-paths ["src" "test/cljs"]
-                 :figwheel {:on-jsload "molsketch-cljs.core-test/on-js-reload"}
-
-                 :compiler {:main molsketch-cljs.core-test
-                            :asset-path "js/compiled/test/out"
-                            :output-to "resources/public/js/compiled/test/test.js"
-                            :output-dir "resources/public/js/compiled/test/out"
-                            :source-map true
-                            :optimizations :none
-                            :source-map-timestamp true}}]}
-
-
-
-  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
-             :css-dirs ["resources/public/css"]}) ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             ; :nrepl-port 7888})
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
+  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "molsketch_cljs" "-r"]})
